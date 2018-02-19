cfacd14ae44783016674d46c21b78ecdadb08fd7
Sarah Spall <sjspall@iu.edu>
2018-02-19 16:42:50 -0500

    connect each target with its corresponding makefile

--- utilities/rusage-parser.rkt
+++ utilities/rusage-parser.rkt
@@ -44,6 +44,34 @@
 (define (considering-target-file? line)
   (helper line (list "Considering" "target" "file")))
 
+(define (invoking-recipe? line)
+  (helper line (list "Invoking" "recipe" "from")))
+
+;; return #f or path
+(define (entering-directory? line)
+  ;; make[n]:
+  (define words (string-split (string-trim line " " #:repeat? #t)))
+  ;; first word should be make something...
+  (cond
+    [(starts-with? (cdr words) (list "Entering" "directory"))
+     ;; get make and number
+     (cons (string-trim (cadr (string-split (string-trim (car words) ":" #:left? #f) "[")) "]" #:left? #f)
+      (string-trim (string-trim (last words) "'" #:left? #f) "`" #:right? #f))]
+    [else
+     #f]))
+
+;; return #f or path
+(define (leaving-directory? line)
+  (define words (string-split (string-trim line " " #:repeat? #t)))
+  ;; first word should be make something...
+  (cond
+    [(starts-with? (cdr words) (list "Leaving" "directory"))
+     ;; get make and number
+     (cons (string-trim (cadr (string-split (string-trim (car words) ":" #:left? #f) "[")) "]" #:left? #f)
+             (string-trim (string-trim (last words) "'" #:left? #f) "`" #:right? #f))]
+    [else
+     #f]))
+
 ;; return #f or target
 (define (finished-prerequisites? line)
   (helper line (list "Finished" "prerequisites" "of" "target" "file")))
@@ -119,17 +147,16 @@
   (define mgraph (create-makegraph))
   (define root (create-target "<ROOT>"))
   (set-makegraph-root! mgraph root)
-  
-  (let read-file ([t root]
-                  [il 0])
-    (define line (read-full-line fip))
+
+  (define (parse-line line ts prqs? dirs)
+    (define t (car ts))
     (cond
       [(equal? "" line)
-       (read-file t il)]
+       (read-file ts prqs? dirs)]
       [(eof-object? line)
        (unless (equal? root t)
          (error 'parse-file "Unexpected end of line before end of target ~a" (target-name t)))]
-
+      
       ;; set field in target appropriately
       [(no-need-to-remake? line) =>
        (lambda (tname)
@@ -137,7 +164,7 @@
            (error 'parse-file "Expected t to be a target"))
          (unless (equal? tname (target-name t))
            (error 'parse-file "Expected no need to remake ~a got ~a" (target-name t) tname))
-         #;(set-target-remake?! t #f))]
+         (read-file (cdr ts) (cdr prqs?) dirs))]
       [(must-remake? line) =>
        (lambda (tname)
          (unless t
@@ -145,14 +172,48 @@
          (unless (equal? tname (target-name t))
            (error 'parse-file "Expected must remake ~a got ~a" (target-name t) tname))
          (set-target-remake?! t #t)
-         (read-file t il))]
+         (read-file ts prqs? dirs))]
+      
+      [(invoking-recipe? line) =>
+       (lambda (tname)
+         (unless t
+           (error 'parse-file "Expected t to be a target"))
+         (unless (equal? tname (target-name t))
+           (error 'parse-file "Expected to invoke recipe for ~a got ~a" (target-name t) tname))
+         
+         ;; next line should be entering directory?
+         (define nline (read-full-line fip))
+         (cond
+           [(entering-directory? nline) =>
+            (lambda (p)
+              (define dir (cdr p))
+              (set-target-mfile! t dir)
+              (read-file ts prqs? (cons p dirs)))]
+           [else
+            (set-target-mfile! t (cdar dirs))
+            (parse-line nline ts prqs? dirs)]))]
+      
+      [(entering-directory? line) =>
+       (lambda (p)
+         (define dir (cdr p))
+         (read-file ts prqs? (cons p dirs)))]
+      [(leaving-directory? line) =>
+       (lambda (p) ;; should identify current dir on top of dirs stack
+         (define n (car p))
+         (define dir (cdr p))
+         (cond
+           [(and (equal? n (caar dirs)) (equal? dir (cdar dirs)))
+            (read-file ts prqs? (cdr dirs))]
+           [else
+            (error 'parse-line "Leaving directory ~a with n ~a but top of stack is ~a\n\n" dir n (car dirs))]))]
       
       [(successfully-remade? line) => ;; finished this target
        (lambda (tname)
          (unless t
            (error 'parse-file "Target is #f\n"))
          (unless (equal? tname (target-name t))
-           (error 'parse-file "Successfully remade target ~a expected to remake target ~a" tname (target-name t))))]
+           (error 'parse-file "Successfully remade target ~a expected to remake target ~a" tname (target-name t)))
+         (read-file (cdr ts) (cdr prqs?) dirs))]
       
       [(finished-prerequisites? line) =>
        (lambda (tname)
@@ -160,27 +221,27 @@
            (error 'parse-file "Target is #f\n"))
          (unless (equal? tname (target-name t))
            (error 'parse-file "Expected to finish prereqs of ~a instead finished prereqs of ~a\n" (target-name t) tname))
-         (read-file t 0))]
+         (read-file ts (cons #f (cdr prqs?)) dirs))]
       
       ;; starting a new target. 
       [(considering-target-file? line) =>
        (lambda (tname)
-         ;; check if target already exists in graph.
-         (define ntarget (cond
-                           [(target-in-graph mgraph tname) =>
-                            identity]
-                           [else
-                            (let ([tmp (create-target tname)])
-                              (add-target-to-makegraph mgraph tmp)
-                              tmp)]))
+         ;; target could be a duplicate. wont know until later.
+         (define ntarget
+           (cond
+             [#f ;(target-in-graph mgraph tname) =>
+              identity]
+             [else
+              (let ([tmp (create-target tname)])
+                (add-target-to-makegraph mgraph tmp)
+                tmp)]))
          
          (when t
-           (if (> il 0) ;; prereq of t?
+           (if (car prqs?) ;; prereq of t?
                (add-dep t ntarget)
                (add-child t ntarget)))
-         (read-file ntarget 1)
-         (read-file t il))]
-
+         (read-file (cons ntarget ts) (cons #t prqs?) dirs))]
+      
       ;; run information
       [(argv? line) =>
        (lambda (cmd)
@@ -192,9 +253,15 @@
                 (add-data-to-target! t info)))]
            [else
             (printf "Expected times line to follow argv line ~a got ~a instead\n" line nline)])
-         (read-file t il))]
+         (read-file ts prqs? dirs))]
       [else
-       (read-file t il)]))
+       (read-file ts prqs? dirs)]))
+  
+  (define (read-file ts prqs? dirs)
+    (define line (read-full-line fip))
+    (parse-line line ts prqs? dirs))
+
+  (read-file (list root) (list #f) (list (cons 0 "top")))
   mgraph)
 
 (define (parse-rusage file-path)
