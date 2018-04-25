52334cfb928ff81e6f1f816d9c914c1f9ba5101d
Sarah Spall <sjspall@iu.edu>
2018-04-25 13:43:37 -0400

    ocaml benchmarking script

---
+++ build-ocaml/make_bench_every_rule.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# version (6.10 etc) | CPU count | rtime | utime | stime
+
+# args
+version=$1
+machine=$2
+MAX=$3
+interval=$4
+makepath=$5
+tarpath=$6
+installprefix=$7
+
+shellpath="/data/beehive/home.local/sjspall/compilation-benchmarks/rusage /bin/bash"
+makeshell="/data/beehive/home.local/sjspall/compilation-benchmarks/make.sh"
+
+path=$(pwd)
+
+tstamp=$(date +%s)
+outfile="$path/results/${tstamp}_${version}_${machine}.out"  # for some reason using version here doesnt work.
+
+# create results directory if it doesnt exist
+mkdir -p $(pwd)/results
+
+# create rusage-out directory if it doesnt exist
+mkdir -p $path/results/rusage-out
+
+touch $outfile
+# write first line to file
+echo "version | CPU count | real | user | sys" >> $outfile
+
+mkdir -p ${makepath}
+
+cd ${makepath}/..
+
+echo "Cleaning"
+
+rm -rf ${makepath}
+
+echo "un-taring"
+
+tar -xzf ${tarpath}
+
+cd ${makepath}
+
+cpu=1
+
+printsfile="$path/results/rusage-out/${tstamp}_${version}_${machine}_$cpu.out"
+
+touch $printsfile
+
+env TMPDIR="/data/beehive/home.local/sjspall/tmp"
+echo "running configure and make"
+
+tout=($( time ((./configure --prefix ${installprefix} &>> $printsfile) && (echo "Toplevel make directory $PWD" &>> $printsfile ) && (make --debug=v MAKE="${makeshell}" SHELL="${shellpath}" -j $cpu world &>> $printsfile ) &&  (make --debug=v MAKE="${makeshell}" SHELL="${shellpath}" -j $cpu install &>> $printsfile)) 2>&1 )) # parens on outside turn output into an array.
+
+rt=${tout[1]} # real time
+ut=${tout[3]} # user time
+st=${tout[5]} # sys  time
+
+echo "make done"
+
+# write result to file
+echo "$version $cpu $rt $ut $st" >> $outfile
+
+cpu=$interval
+
+while [ "$cpu" -le "$MAX" ] # need the spaces
+do
+    # clean
+    echo "Cleaning"
+ 
+    cd ${makepath}/..
+
+    ls
+
+    rm -rf ${makepath}
+
+    ls
+
+    echo "un-taring"
+    tar -xzf ${tarpath}
+
+    cd ${makepath}
+    
+    echo "running configure and make"
+
+    printsfile="$path/results/rusage-out/${tstamp}_${version}_${machine}_$cpu.out"
+
+    touch $printsfile
+
+    env TMPDIR="/data/beehive/home.local/sjspall/tmp"
+    env MAKE="/data/beehive/home.local/sjspall/compilation-benchmarks/make.sh"
+    
+    tout=($( time ((./configure --prefix ${installprefix} &>> $printsfile) && (make --debug=v MAKE="${makeshell}" SHELL="${shellpath}" -j $cpu world &>> $printsfile) &&  (make --debug=v MAKE="${makeshell}" SHELL="${shellpath}" -j $cpu install &>> $printsfile )) 2>&1 )) # parens on outside turn output into an array.
+
+    rt=${tout[1]} # real time
+    ut=${tout[3]} # user time
+    st=${tout[5]} # sys  time
+
+    echo "make done"
+
+    # write result to file
+    echo "$version $cpu $rt $ut $st" >> $outfile
+    
+    cpu=$((cpu + interval))
+done
+
+echo "Done"
+
+    
+
