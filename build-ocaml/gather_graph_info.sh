080adb01faf74c3b653f27f58d9a9d584071ec78
Sarah Spall <sjspall@iu.edu>
2018-05-04 12:35:50 -0400

    created script just for gathering debugging output to use for graph building

---
+++ build-ocaml/gather_graph_info.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# args
+version=$1
+machine=$2
+makepath=$3
+tarpath=$4
+
+# 1. add custom make script to front of path
+export PATH="/data/beehive/home.local/sjspall/compilation-benchmarks:$PATH" 
+# 2. set MAKEJ = 1
+export MAKEJ="1"
+# 3. set output file
+path=$(pwd)
+tstamp=$(date +%s)
+outfile="${path}/../../ocaml-results/rusage-out/${tstamp}_${version}_${machine}.debug" #maybe change extension
+export OUTPUTFILE="${outfile}"
+
+# create results directories if they don't exist
+mkdir -p ${path}/../../ocaml-results
+mkdir -p ${path}/../../ocaml-results/rusage-out
+
+# ocaml specific
+# need a installation prefix if we aren't super user
+installprefix="${path}/../prefix"
+mkdir -p ${installprefix}
+
+# clean things up so we are building fresh code.
+
+mkdir -p ${makepath}
+cd ${makepath}/..
+
+echo "Cleaning"
+rm -rf ${makepath}
+
+echo "un-taring"
+
+tar -xzf ${tarpath}
+cd ${makepath}
+
+# another ocaml specific thing
+env TMPDIR="${path}/tmp"
+
+echo "building"
+
+./configure --prefix ${installprefix} &>> ${OUTPUTFILE} && make world && make install
+
+echo "finished building"
+echo "Debugging output written to ${OUTPUTFILE}"
+
+
