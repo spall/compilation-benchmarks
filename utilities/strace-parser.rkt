373c9b96e10204fa0370d11daf1508ec02ed86ca
Sarah Spall <sjspall@iu.edu>
2018-12-13 20:04:50 -0500

    parse unfinished system calls and parse new strace file extensions with timestamps and counts

--- utilities/strace-parser.rkt
+++ utilities/strace-parser.rkt
@@ -9,11 +9,12 @@
 	 racket/promise
 	 racket/place
 	 racket/match
+	 racket/list
          (prefix-in : parser-tools/lex-sre))
 
 (provide parse-strace)
 
-(define-tokens data (ARGS RETVAL SCALL SIGNAL FINAL))
+(define-tokens data (ARGS UNFINISHED RETVAL SCALL SIGNAL FINAL))
 (define-empty-tokens delim (EOF))
 
 (define strace-lexer
@@ -23,7 +24,11 @@
    ["+++" (token-SIGNAL (string-append lexeme (signal-lexer input-port)))]
    ["---" (token-FINAL (string-append lexeme (final-lexer input-port)))]
    [whitespace (return-without-pos (strace-lexer input-port))]
-   [#\( (token-ARGS (apply string-append ((args-lexer) input-port)))]
+   [#\( (call-with-values (lambda () ((args-lexer) input-port))
+	  (lambda (ls val)
+	    (if (equal? val 'finished)
+		(token-ARGS (apply string-append ls))
+		(token-UNFINISHED (apply string-append ls)))))]
    [#\= (token-RETVAL (apply string-append (retval-lexer input-port)))]
    [(:~ #\() (token-SCALL (apply string-append (cons lexeme (scall-lexer input-port))))]))
 
@@ -48,14 +53,25 @@
 
 (define (args-lexer [pc 0])
   (lexer
-   ["<unfinished ...>" null]
-   [#\" (append (cons lexeme (string-lexer input-port))
-		((args-lexer pc) input-port))]
-   [#\( (cons lexeme ((args-lexer (+ pc 1)) input-port))]
+   ["<unfinished ...>)" (values null 'unfinished)]
+   ["<unfinished ...>" (values null 'unfinished)]
+   [#\" (call-with-values (lambda () (string-lexer input-port))
+	  (lambda (str) 
+	    (call-with-values (lambda () ((args-lexer pc) input-port))
+	      (lambda (rest val)
+		(values (append (cons lexeme str)
+				rest) val)))))]
+   [#\( (call-with-values (lambda () ((args-lexer (+ pc 1)) input-port))
+	  (lambda (rest val)
+	    (values (cons lexeme rest) val)))]
    [#\) (if (= 0 pc)
-            null
-            (cons lexeme ((args-lexer (- pc 1)) input-port)))]
-   [any-char (cons lexeme ((args-lexer pc) input-port))]))
+            (values null 'finished)
+            (call-with-values (lambda () ((args-lexer (- pc 1)) input-port))
+	      (lambda (rest val)
+		(values (cons lexeme rest) val))))]
+   [any-char (call-with-values (lambda () ((args-lexer pc) input-port))
+	       (lambda (rest val)
+		 (values (cons lexeme rest) val)))]))
 
 (define string-lexer
   (lexer
@@ -76,7 +92,7 @@
    (end EOF)
    
    (error (lambda (a name val start end)
-	    'failed))
+	    (error 'parser "failed parsing file ~a on token ~a with value ~a." f name val)))
    
    (tokens data delim)
    
@@ -89,6 +105,8 @@
      [(FINAL) (void)])
     
     (scall
+     [(SCALL UNFINISHED RETVAL) 'failed] ;; ok then
+     [(SCALL UNFINISHED) 'failed]
      [(SCALL ARGS RETVAL) (syscall $1 $2 (string-trim $3))])
     
     (scall-list [() null]
@@ -99,7 +117,6 @@
 ;; parses an strace file
 (define (parse-file f)
   (define file-port (open-input-file f #:mode 'text))
-  
   (begin0 ((strace-parser f) (lambda () (strace-lexer file-port)))
 	  (close-input-port file-port)))
 
@@ -110,28 +127,64 @@
 	  (error 'parse-strace "~a is not a directory\n" dir))
   
   (define scalls (make-hash))
+  (define tstamp->pid (make-hash))
   
   
   (start-workers)
   
+  (define (replace-comma str)
+    (string-replace str "," "."))
+
+  (define count_ 0)
   (define promises (for/list ([f (in-directory dir)])
 			     (define ext (path-get-extension f))
 			     (unless ext
 				     (error 'parse-strace "~a does not have an extension" f))
-			     (define pid (string->number (bytes->string/locale ext #f 1)))
+			     (define strext (bytes->string/locale ext #f 1))
+			     (define pid #f)
+			     (define count #f)
+			     (define tstamp #f)
+			     (define tmp (string-split strext "_")) ;; should be either 2 or 3 things
+			     (set! count_ (+ 1 count_))
+			     (cond
+			      [(= 2 (length tmp)) ;; pid_timestamp
+			       (set! pid (string->number (car tmp)))
+			       (set! tstamp (string->number (replace-comma (cadr tmp))))]
+			      [(= 3 (length tmp))               ;; pid_n_timestamp
+			       (set! pid (string->number (car tmp)))
+			       (set! count (string->number (cadr tmp)))
+			       (set! tstamp (string->number (replace-comma (caddr tmp))))]
+			      [else
+			       (error 'parse-strace "This data is not compatible with current version. ~a" strext)])
 			     
-			     (when (hash-has-key? scalls pid)
-				   (error 'parse-strace "Already have processed syscalls for pid ~a" pid))
+			     (when (hash-ref tstamp->pid tstamp #f)
+				   (error 'parse-strace "Have seen tstamp ~a before" tstamp))
+			     (hash-set! tstamp->pid tstamp (cons pid (if count (+ 1 count) 0)))
 			     
 			     (delay/thread
 			      (let ([res (run-in-place f pid)])
-				(unless (equal? 'failed res)
-					(hash-set! scalls pid res))))))
+				(if (hash-has-key? scalls pid)
+				    (hash-set! scalls pid (hash-set (hash-ref scalls pid) (+ 1 count) (if (contains-failure? res) #f res)))
+				    (hash-set! scalls pid (hash 0 (if (contains-failure? res) #f res))))))
   
   (for ([pr promises])
        (force pr))
   
-  scalls)
+  ;; sort tstamp -> pid 
+  (values (map cdr
+	       (sort (hash->list tstamp->pid)
+		     ;; want a list of pids. 
+		     < #:key car))
+	  scalls))
+
+(define (contains-failure? ls)
+  (cond
+   [(empty? ls)
+    #f]
+   [(equal? 'failure (car ls))
+    #t]
+   [else
+    (contains-failure? (cdr ls))]))
 
 (define WORKERS (make-parameter (and (place-enabled?) (min 16 (processor-count)))))
 
